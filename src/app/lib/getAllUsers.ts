849e664a3d33cb846ca0540fb97ff2c8ebde4106
bipproduction <bip.production.js@gmail.com>
2023-09-14 21:45:34 +0800

    tambahan user name

--- src/app/lib/getAllUsers.ts
+++ src/app/lib/getAllUsers.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from "next/server"
 
 
-export default async function getAllUsers(){
+export default async function getAllUsers() {
     const res = await fetch('http://jsonplaceholder.typicode.com/users')
-    if (!res.ok) {throw new Error ('Fetch Data Gagal')}
-        
-    
-    console.log(res)
-    return NextResponse.json({res,status:200})
+
+    return await res.json()
+
 } 
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+import { useEffect, useState } from "react";
 import getAllUsers from "../lib/getAllUsers";
 import Link from "next/link";
-export default async function UsersPage(){
-    
-
+export default function UsersPage() {
+    const [data, setData] = useState<any[] | null>(null)
+    useEffect(() => {
+        getAllUsers().then((val) => {
+            setData(val)
+        })
+    }, [])
 
     return <>
+        <pre>
+            {JSON.stringify(data, null, 2)}
+        </pre>
     </>
-    
+
 }
